d12c947226a79ab96c98a2ebd49a36624e577802
charlie <3140647@qq.com>
2022-06-17 15:25:23 +0800

    fix log output

--- json/jsoniter.go
+++ json/jsoniter.go
@@ -3,7 +3,14 @@
 
 package json
 
-import jsoniter "github.com/json-iterator/go"
+import (
+	jsoniter "github.com/json-iterator/go"
+	"github.com/json-iterator/go/extra"
+)
+
+func init() {
+	extra.RegisterFuzzyDecoders()
+}
 
 var (
 	json          = jsoniter.ConfigCompatibleWithStandardLibrary
