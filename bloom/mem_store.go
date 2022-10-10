132bb0d0e2049cf64a42ecc6d3fead5c4a416699
charlie <3140647@qq.com>
2022-10-10 11:29:07 +0800

    add lock

--- bloom/mem_store.go
+++ bloom/mem_store.go
@@ -1,24 +1,35 @@
 package bloom
 
-import "github.com/bits-and-blooms/bitset"
+import (
+	"github.com/bits-and-blooms/bitset"
+	"github.com/charlienet/go-mixed/locker"
+)
 
 type memStore struct {
 	size uint
-	set  *bitset.BitSet // 内存位图
+	set  *bitset.BitSet  // 内存位图
+	lock locker.RWLocker // 同步锁
 }
 
 func newMemStore(size uint) *memStore {
 	return &memStore{
 		size: size,
 		set:  bitset.New(size),
+		lock: locker.NewRWLocker(),
 	}
 }
 
 func (s *memStore) Clear() {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
 	s.set.ClearAll()
 }
 
 func (s *memStore) Set(offsets ...uint) error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
 	for _, p := range offsets {
 		s.set.Set(p)
 	}
@@ -27,6 +38,9 @@ func (s *memStore) Set(offsets ...uint) error {
 }
 
 func (s *memStore) Test(offsets ...uint) (bool, error) {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+
 	for _, p := range offsets {
 		if !s.set.Test(p) {
 			return false, nil
