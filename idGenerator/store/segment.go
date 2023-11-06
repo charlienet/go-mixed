330d9f78d3dcac274769752e818ba9a0ad475165
charlie <3140647@qq.com>
2023-11-06 10:09:40 +0800

    添加锁

--- idGenerator/store/segment.go
+++ idGenerator/store/segment.go
@@ -1,6 +1,9 @@
 package store
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 // 号段
 type Segment struct {
@@ -8,22 +11,35 @@ type Segment struct {
 	end     int64
 	current int64
 	reback  bool
+	mu      sync.RWMutex
 }
 
 func (s *Segment) Allot() int64 {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.current++
 	return s.current
 }
 
 func (s *Segment) IsEnding() bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	return (s.current - s.start) > (s.end - s.current)
 }
 
 func (s *Segment) IsEmpty() bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	return s.current == s.end
 }
 
 func (s *Segment) Reback() bool {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	// println("回旋确认:", s.reback, s.current == (s.start+1))
 	return s.reback && s.current == (s.start+1)
 }
